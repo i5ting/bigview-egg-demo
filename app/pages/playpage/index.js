3ed5bfffa9effcd2b33f99990883be802557549f
狼叔 <langshu.ssl@alibaba-inc.com>
2018-07-24 00:51:26 +0800

    refact, please ignore

--- app/pages/playpage/index.js
+++ app/pages/playpage/index.js
@@ -9,20 +9,18 @@ const otherPagelet = require('./other')
 async function index (ctx) {
   // const { ctx } = this;
   this.status = 200
-  const bigView = new BigView(ctx, {
-    layout: layoutPagelet,
-  })
+  const bigView = new BigView(ctx)
 
   // set layout
-  // bigView.layout = layoutPagelet
+  bigView.layout = layoutPagelet
   // set main
   bigView.main = mainPagelet
   // you can custom bigView dataStore
   bigView.dataStore = {}
-
+  // bigView.add(mainPagelet)
   bigView.add(otherPagelet)
 
-  bigView.mode = 'pipeline'
+  // bigView.mode = 'pipeline'
 
   await bigView.start()
 }
